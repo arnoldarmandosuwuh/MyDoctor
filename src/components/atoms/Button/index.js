76cea6ca0dc71ec95cc44ce8dd550f9e04706d79
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2020-08-15 21:03:29 +0700

    slicing Login

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 
-const Button = ({ type, title }) => {
+const Button = ({ type, title, onPress }) => {
     return (
         <TouchableOpacity style={styles.container(type)} onPress={onPress}>
             <Text style={styles.text(type)}>{title}</Text>
